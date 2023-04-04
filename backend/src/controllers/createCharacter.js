d173ae556852261f35a2bf9a57aa62965ebcf909
PatoProgramador <spartan1171947@hotmail.com>
2023-04-03 23:03:53 -0500

    Create character working

--- backend/src/controllers/createCharacter.js
+++ backend/src/controllers/createCharacter.js
@@ -1,10 +1,12 @@
 const { mongoConect } = require("../../config/mongodb");
 const character = require('../../models/Character');
+const middy = require("@middy/core");
+const jsonBodyParser = require("@middy/http-json-body-parser");
 
 const createCharacter = async (event) => {
     if(!event.body) return {
         statusCode: 400,
-        body: JSON.stringify({"error": "Debes pasar los campos necesarios para crear el personaje"});
+        body: JSON.stringify({"error": "Debes pasar los campos necesarios para crear el personaje"})
     };
     const { name, img, status, gender, origin, location  } = event.body;
     try {
@@ -22,13 +24,31 @@ const createCharacter = async (event) => {
         for(const key in validate) {
             const element = validate[key];
             if(!element && key !== "gender" && key !== "location") {
-                
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ "error": `El campo ${key} no puede estar vacío` })
+                };
             }
         };
 
+        const newCharacter = new character(validate);
+        await newCharacter.save();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                "message": `El personaje ${newCharacter.name} fue creado correctamente :)`
+            })
+        };
     } catch (error) {
-        
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ "error": error })
+        };
     }
 };
 
-module.exports = { createCharacter };
+module.exports = {
+    createCharacter: middy(createCharacter)
+    .use(jsonBodyParser()) 
+};
