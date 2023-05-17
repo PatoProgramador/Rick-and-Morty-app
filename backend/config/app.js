9e37089e2b78500ce279addb6ba788327b05a86c
PatoProgramador <spartan1171947@hotmail.com>
2023-05-16 23:19:17 -0500

    Fix app, add midlewares

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,8 +1,20 @@
-import express  from "express";
+import express from "express";
+import morgan from "morgan";
 
-export async function startServer() {
-    let app = express();
-    let PORT = 3500;
+const app = express();
 
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-}
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.use("/", (req, res) => res.send("Hola"))
+
+export default app;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
-import { startServer } from "./config/app.js";
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import app from "./config/app.js";
 import { connectDB } from "./config/mongodb.js";
 import mongoose from "mongoose";
+const PORT = process.env.PORT | 3500
 
 connectDB()
-mongoose.connection.once('open', (err, resp) => {
-    startServer()
+mongoose.connection.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
 });
