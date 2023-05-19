632b5b50f25de68d0be0de01b0d71765eea5d201
PatoProgramador <spartan1171947@hotmail.com>
2023-05-19 16:57:20 -0500

    Api fetch, pending database with location

--- backend/src/controllers/getCharacters.js
+++ backend/src/controllers/getCharacters.js
@@ -1,12 +1,21 @@
 import Character from "../../models/Character.js";
 
+
+
 export const getAllCharacters = async (req, res) => {
     try {
+        // crear todos los character y locations y luego mapearlos para simular relaciones
+        // api characters
+        const url = 'https://rickandmortyapi.com/api/character/'
+        const result = await fetch(url);
+        const jresult = await result.json();
+
         const characters = await Character.find({});
         if (characters.length === 0) {
             throw new Error('No existen personajes en la base de datos aún');
         }
-        return res.status(200).json(characters)
+        // let allChar = [...jresult, ...characters]
+        return res.status(200).json(jresult)
     } catch (error) {
         return res.status(400).json(error.message)
     }
