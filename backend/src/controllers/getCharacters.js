158f20cce44210b78b0b3c56411ee16e205713bf
PatoProgramador <spartan1171947@hotmail.com>
2023-05-18 19:58:13 -0500

    Get by id

--- backend/src/controllers/getCharacters.js
+++ backend/src/controllers/getCharacters.js
@@ -1,12 +1,22 @@
 import Character from "../../models/Character.js";
 
-export const getCharacters = async (req, res) => {
+export const getAllCharacters = async (req, res) => {
     try {
         const characters = await Character.find({});
-        if(characters.length === 0) {
+        if (characters.length === 0) {
             throw new Error('No existen personajes en la base de datos aún');
         }
         return res.status(200).json(characters)
+    } catch (error) {
+        return res.status(400).json(error.message)
+    }
+};
+
+export const getCharacterById = async (req, res) => {
+    const { id } = req.params
+    try {
+        const charac = await Character.findById(id).exec();
+        return res.status(200).json(charac)
     } catch (error) {
         return res.status(400).json(error.message)
     }
--- backend/src/routes/characterRouter.js
+++ backend/src/routes/characterRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getCharacters } from "../controllers/getCharacters.js";
+import { getAllCharacters, getCharacterById } from "../controllers/getCharacters.js";
 import { createCharacter } from "../controllers/createCharacter.js";
 
 const characterRouter = express.Router();
 
-characterRouter.get("/", getCharacters);
+characterRouter.get("/", getAllCharacters);
+characterRouter.get("/:id", getCharacterById);
 characterRouter.post("/", createCharacter);
 
 export default characterRouter;
