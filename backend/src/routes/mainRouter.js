39cbaff4ba1d5f5ecaa96cd927220388f9ab95f0
PatoProgramador <spartan1171947@hotmail.com>
2023-05-19 18:40:23 -0500

    a little example

--- backend/src/routes/mainRouter.js
+++ backend/src/routes/mainRouter.js
@@ -4,6 +4,23 @@ const path = require('path');
 
 const mainRouter = express.Router();
 
+// app.get('/', (req, res) => {
+//     const controllersPath = path.join(__dirname, 'controllers');
+//     fs.readdir(controllersPath, (err, files) => {
+//       if (err) {
+//         console.error('Error al leer las carpetas de los controladores:', err);
+//         res.status(500).send('Error del servidor');
+//         return;
+//       }
+  
+//       const controllerNames = files.map(file => path.parse(file).name);
+//       res.send(controllerNames);
+//     });
+//   });
+  
+//   app.listen(3000, () => {
+//     console.log('Servidor iniciado en http://localhost:3000');
+//   });
 mainRouter.get("/", (req, res) => {
     try {
         return res.status(400).json({"message": "Welcome! "})
