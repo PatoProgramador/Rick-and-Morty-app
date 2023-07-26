f14a5b854f6c247b3d6496040ab71ed630405a7b
PatoProgramador <skpates18@gmail.com>
2023-07-25 19:52:11 -0500

    Move the API to another repo to deploy it

--- API/src/application/models/app.response.ts
+++
@@ -1,8 +0,0 @@
-export class AppResponse<T> {
-  public message = 'success'
-  public response?: T
-
-  constructor(init?: Partial<AppResponse<T>>) {
-    Object.assign(this, init)
-  }
-}
--- API/src/modules/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { characterController } from './character/character.controller'
-
-const router = Router()
-const api = '/api'
-
-characterController({ api, router })
-
-export { router }
